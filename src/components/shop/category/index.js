2964453d583eaa7c4899569e29017cc7a26e84cc
seongmin <berry9901@gmail.com>
2022-08-19 20:11:09 +0900

    fix: shop api connect

--- src/components/shop/category/index.js
+++ src/components/shop/category/index.js
@@ -1,70 +1,79 @@
 import { useEffect, useState } from 'react';
 import { Col, Img, ImgBox, List, Title, Wrapper } from './styled';
 import PLANT_IMG from '../../../assets/imgs/pngwing.com.png';
+import { Link, useLocation, useParams } from 'react-router-dom';
 
 const Category = () => {
-	const [category, setCategory] = useState('all');
-
-	const onClickCategory = e => {
-		e.stopPropagation();
-		setCategory(e.currentTarget.getAttribute('name'));
-	};
-
-	useEffect(() => {
-		console.log(category);
-	}, [category]);
+	const { category } = useParams();
+	console.log(category);
 
 	return (
 		<Wrapper>
 			<List>
-				<Col name="all" onClick={e => onClickCategory(e)}>
-					<ImgBox isfocused={category === 'all'}>
-						<Img src={PLANT_IMG} isFocused={category === 'all'} />
-					</ImgBox>
-					<Title>전체</Title>
-				</Col>
-				<Col onClick={e => onClickCategory(e)} name="flower">
-					<ImgBox>
-						<Img src={PLANT_IMG} />
-					</ImgBox>
-					<Title>꽃</Title>
-				</Col>
-				<Col onClick={e => onClickCategory(e)} name="houseplant">
-					<ImgBox>
-						<Img src={PLANT_IMG} />
-					</ImgBox>
-					<Title>관엽/공기정화</Title>
-				</Col>
-				<Col onClick={e => onClickCategory(e)} name="succulent">
-					<ImgBox>
-						<Img src={PLANT_IMG} />
-					</ImgBox>
-					<Title>다육식물</Title>
-				</Col>
-				<Col onClick={e => onClickCategory(e)} name="wild">
-					<ImgBox>
-						<Img src={PLANT_IMG} />
-					</ImgBox>
-					<Title>야생화/분재</Title>
-				</Col>
-				<Col onClick={e => onClickCategory(e)} name="orchid">
-					<ImgBox>
-						<Img src={PLANT_IMG} />
-					</ImgBox>
-					<Title>동/서양란</Title>
-				</Col>
-				<Col onClick={e => onClickCategory(e)} name="seedling">
-					<ImgBox>
-						<Img src={PLANT_IMG} />
-					</ImgBox>
-					<Title>묘목/씨앗</Title>
-				</Col>
-				<Col onClick={e => onClickCategory(e)} name="etc">
-					<ImgBox>
-						<Img src={PLANT_IMG} />
-					</ImgBox>
-					<Title>기타</Title>
-				</Col>
+				<Link to="VIEW-ALL">
+					<Col>
+						<ImgBox isfocused={category === 'VIEW-ALL'}>
+							<Img src={PLANT_IMG} isFocused={category === 'VIEW-ALL'} />
+						</ImgBox>
+						<Title>전체</Title>
+					</Col>
+				</Link>
+				<Link to="FLOWER">
+					<Col>
+						<ImgBox isfocused={category === 'FLOWER'}>
+							<Img isFocused={category === 'FLOWER'} src={PLANT_IMG} />
+						</ImgBox>
+						<Title>꽃</Title>
+					</Col>
+				</Link>
+				<Link to="FOLIAGE">
+					<Col>
+						<ImgBox isfocused={category === 'FOLIAGE'}>
+							<Img isFocused={category === 'FOLIAGE'} src={PLANT_IMG} />
+						</ImgBox>
+						<Title>관엽/공기정화</Title>
+					</Col>
+				</Link>
+				<Link to="SUCCULENCE">
+					<Col>
+						<ImgBox isfocused={category === 'SUCCULENCE'}>
+							<Img isFocused={category === 'SUCCULENCE'} src={PLANT_IMG} />
+						</ImgBox>
+						<Title>다육식물</Title>
+					</Col>
+				</Link>
+				<Link to="WILD">
+					<Col>
+						<ImgBox isfocused={category === 'WILD'}>
+							<Img isFocused={category === 'WILD'} src={PLANT_IMG} />
+						</ImgBox>
+						<Title>야생화/분재</Title>
+					</Col>
+				</Link>
+				<Link to="ORCHID">
+					<Col>
+						<ImgBox isfocused={category === 'ORCHID'}>
+							<Img isFocused={category === 'ORCHID'} src={PLANT_IMG} />
+						</ImgBox>
+						<Title>동/서양란</Title>
+					</Col>
+				</Link>
+				<Link to="SEED">
+					<Col>
+						<ImgBox isfocused={category === 'SEED'}>
+							<Img isFocused={category === 'SEED'} src={PLANT_IMG} />
+						</ImgBox>
+						<Title>묘목/씨앗</Title>
+					</Col>
+				</Link>
+				<Link to="ELSE">
+					<Col>
+						<ImgBox isfocused={category === 'ELSE'}>
+							<Img isFocused={category === 'ELSE'} src={PLANT_IMG} />
+						</ImgBox>
+						<Title>기타</Title>
+					</Col>
+				</Link>
 			</List>
 		</Wrapper>
 	);
