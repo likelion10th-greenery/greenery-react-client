2c0c73abe2e1a6964355c7eb767b5efcf4eb5e71
seongminn <berry9901@gmail.com>
2022-10-08 20:54:01 +0900

    feat: naver login 구현 (style custom 할 예정)

--- src/App.js
+++ src/App.js
@@ -15,8 +15,13 @@ import Auth from 'pages/Auth';
 import 'primereact/resources/themes/saga-green/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
+import NaverLogin from 'components/Auth/Social/NaverLogin';
 
 function App() {
+	let naver_api_url =
+		'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=7T7w55f77AbZJtSOXk6y&redirect_uri=http://127.0.0.1:8000/accounts/login/naver/callback/&state=' +
+		Math.random().toString(36).substr(3, 14);
+
 	return (
 		<QueryClientProvider client={queryClient}>
 			<GlobalStyle />
